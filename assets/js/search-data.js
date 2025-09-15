1a65743696951e9f94f531bf39b2364be4852138
DelphDem <151145175+DelphDem@users.noreply.github.com>
2025-09-15 09:51:05 +0000

    Deploying to gh-pages from @ GuillaumeCandela/GuillaumeCandela.github.io@083c4540f22af2e376a2e4fa4127d7f62c696673 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,11 +436,6 @@ ninja.data = [{
           description: "Applying Artificial Intelligence to three centuries of socioeconomic colonial history through advanced manuscript analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project",
-          title: 'project',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
