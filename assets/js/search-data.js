e80ed5d5dfe4e34f35890b7942bff805cf960e7a
GuillaumeCandela <guillaume.candela.gc@gmail.com>
2025-09-15 15:26:00 +0000

    Deploying to gh-pages from @ GuillaumeCandela/GuillaumeCandela.github.io@6286d47ffc30987bcb83e9492e1dbb1306ec20ce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "My CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
