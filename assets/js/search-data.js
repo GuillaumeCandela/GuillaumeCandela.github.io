af89abe1fa2323784da0478eb8a87168499714b1
GuillaumeCandela <guillaume.candela.gc@gmail.com>
2026-01-27 10:16:17 +0000

    Deploying to gh-pages from @ GuillaumeCandela/GuillaumeCandela.github.io@9cb406a5842ef22e1d3f35390da7b3e4908d924e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,6 +434,11 @@ ninja.data = [{
           description: "Applying Artificial Intelligence to three centuries of socioeconomic colonial history through advanced manuscript analysis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
+            },},{id: "projects-",
+          title: '',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
             },},{
         id: 'social-bluesky',
         title: 'Bluesky',
